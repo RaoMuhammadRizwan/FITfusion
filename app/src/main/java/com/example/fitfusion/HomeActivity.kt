da8cae0df9394599b1a1c45ca125169e0451fd73
Rao Rizwan <raorixwan69@gmail.com>
2024-05-21 02:27:00 +0500

    This is my progress of Day 1 (20/5/24) it includes splashscreen Activity home acivity login Activity

---
+++ app/src/main/java/com/example/fitfusion/HomeActivity.kt
@@ -0,0 +1,29 @@
+package com.example.fitfusion
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fitfusion.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private val binding: ActivityHomeBinding by lazy{
+        ActivityHomeBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        binding.btnNext.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fitfusion/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.example.fitfusion
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 2000)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
